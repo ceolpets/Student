23b9f8363a50dd67b53339bda8c3f4d40f325772
1014424502 <1014424502@qq.com>
2020-02-28 00:22:03 +0800

    果然还是白白白

--- own.h
+++ own.h
@@ -1,9 +1,19 @@
-class Point
+class Student
 {
+	int no;												//学生的学号
+	string name;										//学生的姓名
+	float score;										//学生的成绩
+	Student* per;										//当前结点指针
+	Student* next;										//下一个结点指针
 public:
-	Point(int = 0, int = 0);
-	static void displayxy(Point p);				//若不给参数，程序区别不出来是哪个对象的非静态成员
-private:
-	int X, Y;
+	Student();											//构造函数
+	Student *find(int i_no);							//查找制定学号的学生
+	void edit(string i_newname, float i_score);			//修改学生的信息
+	void erase();										//删除制定学号的学生
+	int add(Student* i_newStudent);						//增加学生
+	int getno();										//获得学生的学号
+	string getname();									//获得学生的姓名
+	float getscore();									//获得学生的成绩
+	static int maxno;									//当前最大学号
 };
 
